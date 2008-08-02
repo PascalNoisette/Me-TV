996f19667558b7230fe91a5c487726018f9b8e97
Michael Lamothe <michael@sizzles>
2008-08-03 03:07:18 +1000

    Added missing scheduled recording class

---
+++ src/scheduled_recording.cc
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2008 Michael Lamothe
+ *
+ * This file is part of Me TV
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+
+#include "scheduled_recording.h"
+#include "me-tv.h"
+
+Glib::ustring ScheduledRecording::get_start_time_text() const
+{
+	return get_time_text(convert_to_local_time(convert_to_local_time(start_time)), "%c");
+}
+
+Glib::ustring ScheduledRecording::get_duration_text() const
+{
+	Glib::ustring result;
+	guint hours = duration / (60*60);
+	guint minutes = (duration % (60*60)) / 60;
+	if (hours > 0)
+	{
+		result = Glib::ustring::compose("%1 hours", hours);
+	}
+	if (hours > 0 && minutes > 0)
+	{
+		result += ", ";
+	}
+	if (minutes > 0)
+	{
+		result += Glib::ustring::compose("%1 minutes", minutes);
+	}
+	
+	return result;
+}
