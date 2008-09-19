62044dbd69ea0d549ccd79f68a4dcf46c5635fba
Michael Lamothe <michael@sizzles>
2008-09-19 18:43:04 +1000

    Fixed issue with EPG getting wiped on profile save

--- src/data.cc
+++ src/data.cc
@@ -555,7 +555,10 @@ void Data::replace_profile(Profile& profile)
 	while (statement.step() == SQLITE_ROW)
 	{
 		gint channel_id = statement.get_int(0);
-		delete_channel(channel_id);
+		if (profile.find_channel(channel_id) == NULL)
+		{
+			delete_channel(channel_id);
+		}
 	}
 
 	ChannelList& channels = profile.get_channels();
