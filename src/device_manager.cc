7eba33469a59b7d18eee90762d0569ec03a447e6
Michael Lamothe <michael@sizzles>
2008-12-06 23:21:24 +1100

    0.7.6: Disabled ATSC device support until it's working properly

--- src/device_manager.cc
+++ src/device_manager.cc
@@ -100,10 +100,14 @@ gboolean DeviceManager::is_frontend_supported(const Dvb::Frontend& test_frontend
 
 	switch(test_frontend.get_frontend_type())
 	{
-	case FE_OFDM: result = true; break;	// DVB-T
-	case FE_QAM: result = true; break;	// DVB-C
-	case FE_ATSC: result = true; break;
-	default: result = false; break;
+	case FE_OFDM: // DVB-T
+	case FE_QAM: // DVB-C
+//	case FE_ATSC:
+		result = true;
+		break;
+	default:
+		result = false;
+		break;
 	}
 
 	return result;
