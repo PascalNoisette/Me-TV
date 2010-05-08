8d8bc51f8af8b59964c2729500e6139e9219e068
Michael Lamothe <michael@media>
2010-05-08 23:29:30 +1000

    More frontend thread optimisation

--- src/frontend_thread.cc
+++ src/frontend_thread.cc
@@ -106,15 +106,13 @@ void FrontendThread::run()
 		}
 		else
 		{
+			usleep(10000);
+
 			try
 			{
 				Lock lock(mutex, __PRETTY_FUNCTION__);
 
-				if (input_channel->read((gchar*)buffer, TS_PACKET_SIZE * PACKET_BUFFER_SIZE, bytes_read) != Glib::IO_STATUS_NORMAL)
-				{
-					usleep(10000);
-				}
-				else
+				if (input_channel->read((gchar*)buffer, TS_PACKET_SIZE * PACKET_BUFFER_SIZE, bytes_read) == Glib::IO_STATUS_NORMAL)
 				{
 					// Insert PAT/PMT every second second
 					time_t now = time(NULL);
