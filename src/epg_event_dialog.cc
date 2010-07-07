be7b420f54ba45ec2e719601d48bcc3ff99b9307
Michael Lamothe <michael@laptop>
2010-07-07 12:23:56 +1000

    Added Watch Now button

--- src/epg_event_dialog.cc
+++ src/epg_event_dialog.cc
@@ -41,6 +41,7 @@ void EpgEventDialog::show_epg_event(EpgEvent& epg_event)
 	const EpgEventText& epg_event_text = epg_event.get_default_text();
 
 	guint end_time = epg_event.start_time + epg_event.duration;
+	guint now = get_local_time();
 	
 	Glib::ustring information = Glib::ustring::compose(
 	    	"<b>%1</b>\n<b><i>%2</i></b>\n<i>%4 (%5)</i>\n\n%3",
@@ -50,22 +51,26 @@ void EpgEventDialog::show_epg_event(EpgEvent& epg_event)
 		    epg_event.get_start_time_text() + " - " + get_local_time_text(convert_to_utc_time(end_time), "%H:%M"),
 	    	epg_event.get_duration_text());
 	
-	Gtk::Label* label_program_information = NULL;
-	builder->get_widget("label_program_information", label_program_information);
-	label_program_information->set_label(information);
+	Gtk::Label* label_epg_event_information = NULL;
+	builder->get_widget("label_epg_event_information", label_epg_event_information);
+	label_epg_event_information->set_label(information);
 
 	gboolean is_scheduled = scheduled_recording_manager.is_recording(epg_event);
-	Gtk::HBox* hbox_program_dialog_scheduled = NULL;
-	builder->get_widget("hbox_program_dialog_scheduled", hbox_program_dialog_scheduled);
-	hbox_program_dialog_scheduled->property_visible() = is_scheduled;
+	Gtk::HBox* hbox_epg_event_dialog_scheduled = NULL;
+	builder->get_widget("hbox_epg_event_dialog_scheduled", hbox_epg_event_dialog_scheduled);
+	hbox_epg_event_dialog_scheduled->property_visible() = is_scheduled;
 
-	Gtk::Button* button_program_dialog_record = NULL;
-	builder->get_widget("button_program_dialog_record", button_program_dialog_record);
-	button_program_dialog_record->property_visible() = !is_scheduled;
+	Gtk::Button* button_epg_event_dialog_record = NULL;
+	builder->get_widget("button_epg_event_dialog_record", button_epg_event_dialog_record);
+	button_epg_event_dialog_record->property_visible() = !is_scheduled;
 
-	Gtk::Button* button_program_dialog_view_schedule = NULL;
-	builder->get_widget("button_program_dialog_view_schedule", button_program_dialog_view_schedule);
-	button_program_dialog_view_schedule->property_visible() = is_scheduled;
+	Gtk::Button* button_epg_event_dialog_watch_now = NULL;
+	builder->get_widget("button_epg_event_dialog_watch_now", button_epg_event_dialog_watch_now);
+	button_epg_event_dialog_watch_now->property_visible() = epg_event.start_time <= now && now <= end_time;
+
+	Gtk::Button* button_epg_event_dialog_view_schedule = NULL;
+	builder->get_widget("button_epg_event_dialog_view_schedule", button_epg_event_dialog_view_schedule);
+	button_epg_event_dialog_view_schedule->property_visible() = is_scheduled;
 
 	gint result = run();
 	hide();
